21215b2a32e320e90455b17b7b71ae9da7a18c9e
Aditya948351 <adityapatil9721@gmail.com>
2026-01-21 20:07:49 +0530

    UI: Resize logo to 16px height

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                 <div className="container flex justify-between items-center">
                     {/* Logo Section */}
                     <Link to="/" className="flex items-center gap-2 text-decoration-none">
-                        <div className="h-10 w-16 overflow-hidden flex items-center justify-center">
+                        <div className="h-4 w-8 overflow-hidden flex items-center justify-center">
                             <img
                                 src="/travelabroad.png"
                                 alt="Aspire Abroad Logo"
